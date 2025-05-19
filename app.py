0c766484820f48fe3d5fad0978552bbb055904aa
himanshi-paraste <146969243+himanshi-paraste@users.noreply.github.com>
2025-05-19 22:40:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ def home():
     a = 5
     b = 7
     result = a + b
-    return f"<h1>Addition Result:</h1><p>{a} + {b} = {result}</p>"
+    return f"""
+        <h1>Hello from Jenkins CI/CD pipeline test!</h1>
+        <h2>Addition Result:</h2>
+        <p>{a} + {b} = {result}</p>
+    """
 
 if __name__ == '__main__':
-    print("Hello from Jenkins CI/CD pipeline test!")  # 👈 testing 
+    print("This message will show in terminal logs")
     app.run(host='0.0.0.0', port=5000)
