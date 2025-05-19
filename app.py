81ddb7e25e3d065411d473b0b0d3f0f589109d2c
himanshi-paraste <146969243+himanshi-paraste@users.noreply.github.com>
2025-05-19 22:23:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,5 @@ def home():
     return f"<h1>Addition Result:</h1><p>{a} + {b} = {result}</p>"
 
 if __name__ == '__main__':
+    print("Hello from Jenkins CI/CD pipeline test!")  # 👈 testing 
     app.run(host='0.0.0.0', port=5000)
