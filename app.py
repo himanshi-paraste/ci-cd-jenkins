345c278503b4118ce7ec593900edddbee0f2f409
himanshi-paraste <146969243+himanshi-paraste@users.noreply.github.com>
2025-05-23 03:37:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def home():
     a = 5
     b = 7
     result = a + b
-    return f"<h1>Addition aim Result:</h1><p>{a} + {b} = {result}</p>"
+    return f"<h1>Addition  Result:</h1><p>{a} + {b} = {result}</p>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
